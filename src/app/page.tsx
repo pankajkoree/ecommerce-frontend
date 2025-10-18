d9a1efb3c6776a90999b3ee9e1bbab5ba7f55f7a
pankajkoree <pankajkori2057@gmail.com>
2025-10-18 22:49:12 +0545

    ecommerce frontend

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Header from "@/components/Header";
 
 export default function Home() {
   return (
-    <div className="flex flex-col gap-8 p-4 border-8 border-purple-300">
+    <div className="flex max-h-screen flex-col gap-8 p-4 border-8 border-purple-300">
       {/* header */}
       <Header />
       {/* header end */}
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,24 +1,54 @@
 import Image from "next/image";
+import Link from "next/link";
+import { useEffect, useState } from "react";
 
 const Header = () => {
+  const [isDark, setIsDark] = useState(() => {
+    if (typeof window !== "undefined") {
+      return localStorage.getItem("theme") === "dark";
+    }
+    return false;
+  });
+
+  useEffect(() => {
+    document.documentElement.classList.toggle("dark", isDark);
+
+    try {
+      localStorage.setItem("theme", isDark ? "dark" : "light");
+    } catch (error) {
+      console.warn("unable to access localstorage : ", error);
+    }
+    const appliedTheme = localStorage.getItem("theme");
+    if (appliedTheme === "dark") {
+      document.documentElement.classList.add("dark");
+      setIsDark(true);
+    }
+  }, [isDark]);
+
+  const changeTheme = () => setIsDark((old) => !old);
   return (
-    <nav className="flex justify-between">
+    <div className="flex justify-between">
       <h1>DummyProducts</h1>
-      <ul className="flex gap-8">
-        <link href="/">Home</link>
-        <link href="/docs">Docs</link>
-        <link href="">Github</link>
-        <li>
-          <Image
-            src="/theme.png"
-            width={20}
-            height={20}
-            alt="theme"
-            className="hover:cursor-pointer"
-          />
-        </li>
-      </ul>
-    </nav>
+      <nav className="flex gap-8">
+        <Link href="/">
+          <p>Home</p>
+        </Link>
+        <Link href="/docs">
+          <p>Docs</p>
+        </Link>
+        <Link href="https://github.com/pankajkoree/ecommerce-frontend">
+          <p>Github</p>
+        </Link>
+        <Image
+          src="/theme.png"
+          width={20}
+          height={20}
+          alt="theme"
+          className="hover:cursor-pointer"
+          onClick={changeTheme}
+        />
+      </nav>
+    </div>
   );
 };
 
